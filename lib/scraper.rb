251874a2875c99c36fc0fc47c2baf9097ac26932
Yarvin Hernandez <yarhumher@hotmail.com>
2020-05-29 03:13:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(profile_url))
+   doc = Nokogiri::HTML(open(profile_url))
    student_links = doc.css("a").map{|link| link['href']}
    quote = doc.css(".profile-quote").text
    student_links.delete("../")
